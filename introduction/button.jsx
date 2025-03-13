9ba0b84c14da4727906f98310ea4821ddcc53d24
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-03-14 03:09:51 +0530

    introduction, Dynamic and reuseable component

---
+++ introduction/button.jsx
@@ -0,0 +1,4 @@
+function Button() {
+  return <button type="submit">submit</button>
+}
+export default Button
---
+++ introduction/src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello() {
+  let myName = "ashamin";
+  function fullName() {
+    return "Ashamin Biswas"
+  }
+  return <h3>Hello Everyone, my name is {fullName()} </h3>
+}
+
+export default Hello
